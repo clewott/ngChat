66e481c0b26189c5e6a0f5094ec7d408909badbe
Ansley Jones <ansleyjones@Ansleys-MacBook-Pro.local>
2014-09-14 11:36:45 -0400

    working chat app! unstyled

--- public/js/chat/chatCtrl.js
+++ public/js/chat/chatCtrl.js
@@ -1,21 +1,26 @@
 'use strict';
 
 angular.module('chatApp')
-  .controller('chatCtrl', function ($scope, $location, $rootScope, $routeParams, $timeout, $cookies, $cookieStore, chatSvc, userSvc) {
+  .controller('chatCtrl', function ($scope, $location, $rootScope, $routeParams, $interval, $cookies, $cookieStore, chatSvc) {
 
 
     $scope.user = chatSvc.userName;
     console.log($scope.user);
 
-    chatSvc.getChats().success(function(chats){
+    $scope.getChats = $interval(function()
+    {
+      chatSvc.getChats().success(function(chats){
       $scope.chats = chats;
-    });
+      });
+    }, 500);
 
 
     $scope.addUser = function(userName){
       chatSvc.addUser(userName);
       $location.path("/chat");
     };
+
+
 
     $scope.addChat = function(chat) {
       var chat =
@@ -28,68 +33,4 @@ angular.module('chatApp')
       $scope.submitChat ={};
     };
 
-                    // I keep track of how many times the user has clicked
-                // the link.
-                $scope.clickCount = 0;
-
-                // I determine if the "encouraging" feedback is shown.
-                $scope.isShowingFeedback = false;
-
-
-                // ---
-                // WATCHERS.
-                // ---
-
-
-                // When you call the $watch() method, AngularJS
-                // returns an unbind function that will kill the
-                // $watch() listener when its called.
-                var unbindWatcher = $scope.$watch(
-                    "clickCount",
-                    function( newClickCount ) {
-
-                        console.log( "Watching click count." );
-
-                        if ( newClickCount >= 5 ) {
-
-                            $scope.isShowingFeedback = true;
-
-                            // Once the feedback has been displayed,
-                            // there's no more need to watch the change
-                            // in the model value.
-                            unbindWatcher();
-
-                        }
-
-                    }
-                );
-
-
-                // ---
-                // PUBLIC METHODS.
-                // ---
-
-
-                // I increment the click count.
-                $scope.incrementCount = function() {
-
-                    $scope.clickCount++;
-
-                    // NOTE: You could just as easily have called a
-                    // counter-related method right here to test when
-                    // to show feedback. I am just demonstrating an
-                    // alternate approach.
-
-                };
-
-            
-
-
-
-
-    // $rootScope.$on("chat:added", function(){
-    //   chatSvc.getChat().success(function(chat){
-    //     $scope.singleChat = chat;
-    //   });
-    // });
 });
