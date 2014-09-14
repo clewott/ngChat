d1eccf973733cc2e3005b924063b2f4781dfaa77
Ansley Jones <ansleyjones@Ansleys-MacBook-Pro.local>
2014-09-14 11:03:07 -0400

    watch test

--- public/js/chat/chatCtrl.js
+++ public/js/chat/chatCtrl.js
@@ -28,6 +28,65 @@ angular.module('chatApp')
       $scope.submitChat ={};
     };
 
+                    // I keep track of how many times the user has clicked
+                // the link.
+                $scope.clickCount = 0;
+
+                // I determine if the "encouraging" feedback is shown.
+                $scope.isShowingFeedback = false;
+
+
+                // ---
+                // WATCHERS.
+                // ---
+
+
+                // When you call the $watch() method, AngularJS
+                // returns an unbind function that will kill the
+                // $watch() listener when its called.
+                var unbindWatcher = $scope.$watch(
+                    "clickCount",
+                    function( newClickCount ) {
+
+                        console.log( "Watching click count." );
+
+                        if ( newClickCount >= 5 ) {
+
+                            $scope.isShowingFeedback = true;
+
+                            // Once the feedback has been displayed,
+                            // there's no more need to watch the change
+                            // in the model value.
+                            unbindWatcher();
+
+                        }
+
+                    }
+                );
+
+
+                // ---
+                // PUBLIC METHODS.
+                // ---
+
+
+                // I increment the click count.
+                $scope.incrementCount = function() {
+
+                    $scope.clickCount++;
+
+                    // NOTE: You could just as easily have called a
+                    // counter-related method right here to test when
+                    // to show feedback. I am just demonstrating an
+                    // alternate approach.
+
+                };
+
+            
+
+
+
+
     // $rootScope.$on("chat:added", function(){
     //   chatSvc.getChat().success(function(chat){
     //     $scope.singleChat = chat;
